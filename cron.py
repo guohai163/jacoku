ad7481bb87e3c512efe0c1198d37ddb2f4ad7dd0
guohai <guohai@gmail.com>
2025-01-30 22:10:33 +0800

    完善了一下cron初始化脚本

--- cron.py
+++ cron.py
@@ -15,25 +15,32 @@ def init_cron_task():
     """
     cron_manager = CronTab(user=True)
     # 写定时任务,
-    mail_job = cron_manager.new(
+    jacoco_job = cron_manager.new(
         command='. /etc/environment;python3 /opt/jacoco-data/main.py >> /var/log/git_backup.log 2>&1')
-    mail_job.setall('0 1 * * *')
+    cron_parm = os.getenv('CRON')
+    if cron_parm is None:
+        jacoco_job.setall('0 1 * * *')
+        LOG.info('If the cron parameter is empty, the default time is 1 o\'clock')
+    else:
+        jacoco_job.setall(cron_parm)
     cron_manager.write()
+
+def env_check():
+    minio_url = os.getenv('MINIO_URL')
+    minio_access_key = os.getenv('MINIO_ACCESS')
+    minio_secret_key = os.getenv('MINIO_SECRET')
+    if minio_url is None or minio_access_key is None or minio_secret_key is None:
+        LOG.error('The required parameters are empty')
+        sys.exit(2)
 
 
 def main():
-    try:
-        optlist, args = getopt.getopt(sys.argv[1:], 'hlt:', ['back', 'init'])
-    except getopt.GetoptError as err:
-        LOG.error(str(err))
-        sys.exit(2)
-    for o, a in optlist:
-        if o in ('-i', '--init'):
-            init_cron_task()
-            while True:
-                time.sleep(60)
-                LOG.info('sleep 60s')
+    init_cron_task()
+    while True:
+        time.sleep(60)
+        LOG.info('sleep 60s')
 
 
 if __name__ == '__main__':
+    env_check()
     main()
--- main.py
+++ main.py
@@ -8,8 +8,29 @@ from minio import Minio
 import time
 import uuid
 
+minio_client = Minio(os.getenv('MINIO_URL'),
+                     access_key=os.getenv('MINIO_ACCESS'),
+                     secret_key=os.getenv('MINIO_SECRET'),
+                     )
 
+bucket_name = "jacoco-report"
+local_base_dir = '/tmp/code_repo/'
+
+path_date = time.strftime("%Y-%m-%d", time.localtime())
+maven_path = "/opt/apache-maven-3.9.9/bin"
+
+jacoco_cli = "/opt/org.jacoco.cli-0.8.12-nodeps.jar"
+
+git_commit_dic = {}
+
+def check_minio():
+    found = minio_client.bucket_exists(bucket_name)
+    if not found:
+        minio_client.make_bucket(bucket_name)
+        print("Created bucket", bucket_name)
+    else:
+        print("Bucket", bucket_name, "already exists")
 
 if __name__ == '__main__':
     print('jacoco-report start ...')
-    print(os.getenv('MINIO_URL'))
+    check_minio()
